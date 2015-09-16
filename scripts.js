a5790ee16c0de24412050bfd340b237eed98b36d
Nikolai Sander <nikolai@eodsoft.com>
2015-09-15 18:09:47 -0700

    table

--- scripts.js
+++ scripts.js
@@ -4,22 +4,20 @@ $(document).ready(function(){
         //alert(element);
 
         var appid = appHeading.id;
-        //alert(appid);
 
         $.getJSON("https://itunes.apple.com/lookup/json?id=" + appid + "&callback=?", function( json ) {
-            appHeading.innerHTML = appHeading.innerHTML + ":&nbsp&nbsp&nbsp&nbsp" + (json.results[0].fileSizeBytes/1024/1024).toPrecision(2) + " MB " + "&nbsp&nbsp&nbsp&nbsp&nbspversion: "+json.results[0].version + "&nbsp&nbsp&nbsp&nbsp&nbspavg rating:&nbsp&nbsp"+json.results[0].averageUserRatingForCurrentVersion + "&nbsp&nbsp&nbsp&nbsp&nbspminOS&nbsp&nbsp: "+json.results[0].minimumOsVersion + "&nbsp&nbsp&nbsp&nbsp&nbspratingCount tot/cur :&nbsp&nbsp"+json.results[0].userRatingCount+" / "+json.results[0].userRatingCountForCurrentVersion;
+            //appHeading.innerHTML = appHeading.innerHTML + ":&nbsp&nbsp&nbsp&nbsp" + (json.results[0].fileSizeBytes/1024/1024).toPrecision(2) + " MB " + "&nbsp&nbsp&nbsp&nbsp&nbspversion: "+json.results[0].version + "&nbsp&nbsp&nbsp&nbsp&nbspavg rating:&nbsp&nbsp"+json.results[0].averageUserRatingForCurrentVersion + "&nbsp&nbsp&nbsp&nbsp&nbspminOS&nbsp&nbsp: "+json.results[0].minimumOsVersion + "&nbsp&nbsp&nbsp&nbsp&nbspratingCount tot/cur :&nbsp&nbsp"+json.results[0].userRatingCount+" / "+json.results[0].userRatingCountForCurrentVersion;
+            appHeading.innerHTML = appHeading.innerHTML + "<td>"+(json.results[0].fileSizeBytes/1024/1024).toPrecision(2) + " MB "+"<\/td>" + "<td>"+json.results[0].version+"<\/td> <td>"+json.results[0].averageUserRatingForCurrentVersion+"<\/td> <td>"+json.results[0].minimumOsVersion+"<\/td> <td>"+json.results[0].userRatingCount+"<\/td>" + "<td>"+json.results[0].userRatingCountForCurrentVersion+"<\/td>";
         });
 
     }
 
    var myalert = function() {
        // get an array of child nodes
-       var divChildren = $('#Apps').children();
+       var rows = $('#Apps').children()[0].rows;
 
-
-       for (var i=0; i<divChildren.length; i++) {
-           //alert(divChildren[i]);
-           printSize(divChildren[i]);
+       for (var i=0; i<rows.length; i++) {
+           printSize(rows[i]);
        }
 
    	}
